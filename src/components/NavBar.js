a6deeb6ab0e894dc0b53dbe90bee01c1fcdf235f
Loae Monla Ahmed <lueyahmad@gmail.com>
2020-11-01 15:35:22 +0000

    Automatically backed up by Learn

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,11 +5,20 @@ const NavBar = () => {
   return (
       
       {/*{code here}*/}
+<<<<<<< HEAD
     <div className="navbar">
       <NavLink to="/">Home</NavLink> 
       <NavLink to="/movies">Movies</NavLink>
       <NavLink to="/directors">Directors</NavLink>
       <NavLink to="/actors">Actors</NavLink>
+=======
+      <div className="navbar">
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/actors">Actors</NavLink>
+      <NavLink to="/directors">Directors</NavLink>
+      <NavLink to="/movies">Movies</NavLink>
+    </div>
+>>>>>>> 5746d1262c10780775ca0e61c3eb3129ac29864c
     </div>
 };
 
